cd0f448121c4b7fbc2aaf4190f4558c2a798fc59
arthurshakov <33399638+arthurshakov@users.noreply.github.com>
2025-09-03 11:39:44 +0300

    добавил эффект звуковой волны, сделал рефакторинг

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -20,6 +20,10 @@ export default {
 		'node_modules',
 	],
 
+	modulePaths: [
+		'<rootDir>src/',
+	],
+
 	// An array of file extensions your modules use
 	moduleFileExtensions: [
 		'js',
@@ -39,6 +43,14 @@ export default {
 		// "**/__tests__/**/*.[jt]s?(x)",
 		// "**/?(*.)+(spec|test).[tj]s?(x)"
 	],
+
+	// A list of paths to modules that run some code to configure or set up the testing framework before each test
+	setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+
+	// A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+	moduleNameMapper: {
+		'\\.s?css$': 'identity-obj-proxy',
+	},
 
 	// All imported modules in your tests should be mocked automatically
 	// automock: false,
@@ -93,9 +105,6 @@ export default {
 	// The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
 	// maxWorkers: "50%",
 
-	// A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-	// moduleNameMapper: {},
-
 	// An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
 	// modulePathIgnorePatterns: [],
 
@@ -136,9 +145,6 @@ export default {
 
 	// The paths to modules that run some code to configure or set up the testing environment before each test
 	// setupFiles: [],
-
-	// A list of paths to modules that run some code to configure or set up the testing framework before each test
-	// setupFilesAfterEnv: [],
 
 	// The number of seconds after which a test is considered as slow and reported as such in the results.
 	// slowTestThreshold: 5,
