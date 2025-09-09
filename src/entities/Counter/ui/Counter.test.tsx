ba49cbf047be7011ab82e357dbb506efd72322f7
arthurshakov <33399638+arthurshakov@users.noreply.github.com>
2025-09-09 21:44:35 +0300

    added redux store and Counter entity

---
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -0,0 +1,46 @@
+import { screen } from '@testing-library/react';
+import { userEvent } from '@storybook/testing-library';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
+import { Counter } from './Counter';
+
+describe('Counter', () => {
+	test('Counter is in the document', () => {
+		componentRender(<Counter />, {
+			initialState: {
+				counter: {
+					value: 10,
+				},
+			},
+		});
+
+		expect(screen.getByTestId('value-title')).toHaveTextContent('10');
+	});
+
+	test('increment', () => {
+		componentRender(<Counter />, {
+			initialState: {
+				counter: {
+					value: 10,
+				},
+			},
+		});
+
+		userEvent.click(screen.getByTestId('increment-button'));
+
+		expect(screen.getByTestId('value-title')).toHaveTextContent('11');
+	});
+
+	test('decrement', () => {
+		componentRender(<Counter />, {
+			initialState: {
+				counter: {
+					value: 10,
+				},
+			},
+		});
+
+		userEvent.click(screen.getByTestId('decrement-button'));
+
+		expect(screen.getByTestId('value-title')).toHaveTextContent('9');
+	});
+});
