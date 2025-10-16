9da85057a2b03b5d4ad1f36aed77ea7ee9fbc2af
arthurshakov <33399638+arthurshakov@users.noreply.github.com>
2025-10-16 21:06:15 +0300

    added popover and notifications

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,12 +3,13 @@ import { useTranslation } from 'react-i18next';
 import React, { memo, useCallback, useState } from 'react';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { LoginModal } from 'features/AuthByUserName';
+import { AvatarDropdown } from 'features/avatarDropdown';
+import { NotificationButton } from 'features/notificationButton';
 import { useDispatch, useSelector } from 'react-redux';
 import { Text, TextTheme } from 'shared/ui/Text/Text';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
-import { Dropdown } from 'shared/ui/Dropdown/Dropdown';
-import { Avatar } from 'shared/ui/Avatar/Avatar';
+import { HStack } from 'shared/ui/Stack';
 import {
 	getUserAuthData,
 	isUserAdmin,
@@ -58,25 +59,12 @@ export const Navbar = memo(({ className }: NavbarProps) => {
 				>
 					{t('Создать статью')}
 				</AppLink>
-				<Dropdown
-					direction="bottom left"
-					className={cls.dropdown}
-					items={[
-						...(isAdminPanelAvailable ? [{
-							content: t('Админка'),
-							href: RoutePath.admin_panel,
-						}] : []),
-						{
-							content: t('Профиль'),
-							href: RoutePath.profile + authData.id,
-						},
-						{
-							content: t('Выйти'),
-							onClick: onLogout,
-						},
-					]}
-					trigger={<Avatar size={30} src={authData.avatar} />}
-				/>
+
+				<HStack gap="16" className={cls.actions}>
+					<NotificationButton />
+
+					<AvatarDropdown />
+				</HStack>
 			</header>
 		);
 	}
