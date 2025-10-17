621e68ee819ff23fd624adac95adf9d27e54b87c
arthurshakov <33399638+arthurshakov@users.noreply.github.com>
2025-10-17 20:33:02 +0300

    added drawer and device detector

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,61 +1,59 @@
 import { classNames, Mods } from 'shared/lib/classNames/classNames';
-import {
-	ReactNode, MouseEvent, useState, useRef,
-	useEffect,
-	useCallback,
+import React, {
 	MutableRefObject,
+	ReactNode, useCallback, useEffect, useRef, useState,
 } from 'react';
-import cls from './Modal.module.scss';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { Overlay } from '../Overlay/Overlay';
 import { Portal } from '../Portal/Portal';
+import cls from './Modal.module.scss';
 
 interface ModalProps {
-	className?: string;
-	children?: ReactNode;
-	isOpen?: boolean;
-	onClose?: () => void;
-	lazy?: boolean;
+    className?: string;
+    children?: ReactNode;
+    isOpen?: boolean;
+    onClose?: () => void;
+    lazy?: boolean;
 }
 
 const ANIMATION_DELAY = 300;
 
-export const Modal = ({
-	className, children, isOpen, onClose, lazy,
-}: ModalProps) => {
-	const [modalIsClosing, setModalIsClosing] = useState(false);
+export const Modal = (props: ModalProps) => {
+	const {
+		className,
+		children,
+		isOpen,
+		onClose,
+		lazy,
+	} = props;
+
+	const [isClosing, setIsClosing] = useState(false);
 	const [isMounted, setIsMounted] = useState(false);
 	const timerRef = useRef() as MutableRefObject<ReturnType<typeof setTimeout>>;
-
-	const mods: Mods = {
-		[cls.opened]: isOpen,
-		[cls.isClosing]: modalIsClosing,
-	};
-
-	const closeHandler = useCallback(() => {
-		if (onClose) {
-			setModalIsClosing(true);
-
-			timerRef.current = setTimeout(() => {
-				onClose();
-				setModalIsClosing(false);
-			}, ANIMATION_DELAY);
-		}
-	}, [onClose]);
-
-	const onContentClick = (e: MouseEvent) => {
-		e.stopPropagation();
-	};
-
-	const onKeyDown = useCallback((e: KeyboardEvent) => {
-		if (e.key === 'Escape') {
-			closeHandler();
-		}
-	}, [closeHandler]);
+	const { theme } = useTheme();
 
 	useEffect(() => {
 		if (isOpen) {
 			setIsMounted(true);
 		}
 	}, [isOpen]);
+
+	const closeHandler = useCallback(() => {
+		if (onClose) {
+			setIsClosing(true);
+			timerRef.current = setTimeout(() => {
+				onClose();
+				setIsClosing(false);
+			}, ANIMATION_DELAY);
+		}
+	}, [onClose]);
+
+	// Новые ссылки!!!
+	const onKeyDown = useCallback((e: KeyboardEvent) => {
+		if (e.key === 'Escape') {
+			closeHandler();
+		}
+	}, [closeHandler]);
 
 	useEffect(() => {
 		if (isOpen) {
@@ -68,16 +66,22 @@ export const Modal = ({
 		};
 	}, [isOpen, onKeyDown]);
 
+	const mods: Mods = {
+		[cls.opened]: isOpen,
+		[cls.isClosing]: isClosing,
+	};
+
 	if (lazy && !isMounted) {
 		return null;
 	}
 
 	return (
 		<Portal>
-			<div className={classNames(cls.Modal, mods, [className])}>
-				<div className={cls.overlay} onClick={closeHandler} />
-
-				<div className={cls.content} onClick={onContentClick}>
+			<div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
+				<Overlay onClick={closeHandler} />
+				<div
+					className={cls.content}
+				>
 					{children}
 				</div>
 			</div>
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,19 +3,14 @@ import { useTranslation } from 'react-i18next';
 import React, { memo, useCallback, useState } from 'react';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { LoginModal } from 'features/AuthByUserName';
-import { AvatarDropdown } from 'features/avatarDropdown';
-import { NotificationButton } from 'features/notificationButton';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Text, TextTheme } from 'shared/ui/Text/Text';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import { HStack } from 'shared/ui/Stack';
-import {
-	getUserAuthData,
-	isUserAdmin,
-	isUserManager,
-	userActions,
-} from '../../../entities/User';
+import { NotificationButton } from 'features/notificationButton';
+import { AvatarDropdown } from 'features/avatarDropdown';
+import { getUserAuthData } from '../../../entities/User';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -26,9 +21,6 @@ export const Navbar = memo(({ className }: NavbarProps) => {
 	const { t } = useTranslation();
 	const [isAuthModal, setIsAuthModal] = useState(false);
 	const authData = useSelector(getUserAuthData);
-	const dispatch = useDispatch();
-	const isAdmin = useSelector(isUserAdmin);
-	const isManager = useSelector(isUserManager);
 
 	const onCloseModal = useCallback(() => {
 		setIsAuthModal(false);
@@ -37,12 +29,6 @@ export const Navbar = memo(({ className }: NavbarProps) => {
 	const onShowModal = useCallback(() => {
 		setIsAuthModal(true);
 	}, []);
-
-	const onLogout = useCallback(() => {
-		dispatch(userActions.logout());
-	}, [dispatch]);
-
-	const isAdminPanelAvailable = isAdmin || isManager;
 
 	if (authData) {
 		return (
@@ -59,10 +45,8 @@ export const Navbar = memo(({ className }: NavbarProps) => {
 				>
 					{t('Создать статью')}
 				</AppLink>
-
 				<HStack gap="16" className={cls.actions}>
 					<NotificationButton />
-
 					<AvatarDropdown />
 				</HStack>
 			</header>
