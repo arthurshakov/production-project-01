c2558405ea7142281de74aed98072da6b2160a04
arthurshakov <33399638+arthurshakov@users.noreply.github.com>
2025-10-20 19:27:32 +0300

    updated custom eslint-plugin, fixed an eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -69,7 +69,7 @@ module.exports = {
 		'no-param-reassign': 'off',
 		'no-undef': 'off',
 		'react/no-array-index-key': 'off',
-		'artur11/path-checker': 'error',
+		'artur11/path-checker': ['error', { alias: '@' }],
 	},
 	globals: {
 		__IS_DEV__: true,
