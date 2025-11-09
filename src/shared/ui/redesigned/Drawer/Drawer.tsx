1e6d81d5c452b467ad403d33d64209bb037d27d2
arthurshakov <33399638+arthurshakov@users.noreply.github.com>
2025-11-09 21:10:50 +0300

    redesigned modal and drawer

--- src/shared/ui/redesigned/Drawer/Drawer.tsx
+++ src/shared/ui/redesigned/Drawer/Drawer.tsx
@@ -4,10 +4,11 @@ import {
 	AnimationProvider,
 	useAnimationLibs,
 } from '@/shared/lib/components/AnimationProvider';
-import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
-import { Overlay } from '../../redesigned/Overlay/Overlay';
+import { Overlay } from '../Overlay/Overlay';
 import cls from './Drawer.module.scss';
-import { Portal } from '../../redesigned/Portal/Portal';
+import { Portal } from '../Portal/Portal';
+import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
+import { toggleFeatures } from '@/shared/lib/features';
 
 interface DrawerProps {
 	className?: string;
@@ -83,14 +84,27 @@ export const DrawerContent = memo((props: DrawerProps) => {
 	const display = y.to((py) => (py < height ? 'block' : 'none'));
 
 	return (
-		<Portal>
+		<Portal element={document.getElementById('app') ?? document.body}>
 			<div
-				className={classNames(cls.Drawer, {}, [className, theme, 'app_drawer'])}
+				className={classNames(cls.Drawer, {}, [
+					className,
+					theme,
+					'app_drawer',
+					toggleFeatures({
+						name: 'isAppRedesigned',
+						on: () => cls.drawerNew,
+						off: () => cls.drawerOld,
+					}),
+				])}
 			>
 				<Overlay onClick={close} />
 				<Spring.a.div
 					className={cls.sheet}
-					style={{ display, bottom: `calc(-100vh + ${height - 100}px)`, y }}
+					style={{
+						display,
+						bottom: `calc(-100vh + ${height - 100}px)`,
+						y,
+					}}
 					{...bind()}
 				>
 					{children}
--- src/shared/ui/redesigned/Modal/Modal.tsx
+++ src/shared/ui/redesigned/Modal/Modal.tsx
@@ -1,10 +1,11 @@
 import React, { ReactNode } from 'react';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import { useModal } from '@/shared/lib/hooks/useModal/useModal';
-import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
-import { Overlay } from '../../redesigned/Overlay/Overlay';
-import { Portal } from '../../redesigned/Portal/Portal';
+import { Overlay } from '../Overlay/Overlay';
+import { Portal } from '../Portal/Portal';
 import cls from './Modal.module.scss';
+import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
+import { toggleFeatures } from '@/shared/lib/features';
 
 interface ModalProps {
 	className?: string;
@@ -28,6 +29,7 @@ export const Modal = (props: ModalProps) => {
 		onClose,
 		isOpen,
 	});
+
 	const { theme } = useTheme();
 
 	const mods: Mods = {
@@ -40,9 +42,18 @@ export const Modal = (props: ModalProps) => {
 	}
 
 	return (
-		<Portal>
+		<Portal element={document.getElementById('app') ?? document.body}>
 			<div
-				className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}
+				className={classNames(cls.Modal, mods, [
+					className,
+					theme,
+					'app_modal',
+					toggleFeatures({
+						name: 'isAppRedesigned',
+						on: () => cls.modalNew,
+						off: () => cls.modalOld,
+					}),
+				])}
 			>
 				<Overlay onClick={close} />
 				<div className={cls.content}>{children}</div>
