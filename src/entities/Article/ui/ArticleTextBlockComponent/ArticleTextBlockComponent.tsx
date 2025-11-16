3a34d97f396e54f4edd383e3077759a3906aa896
arthurshakov <33399638+arthurshakov@users.noreply.github.com>
2025-11-16 13:22:13 +0300

    fixed the key prop errors

--- src/entities/Article/ui/ArticleTextBlockComponent/ArticleTextBlockComponent.tsx
+++ src/entities/Article/ui/ArticleTextBlockComponent/ArticleTextBlockComponent.tsx
@@ -31,6 +31,7 @@ export const ArticleTextBlockComponent = memo(
 				{block.paragraphs.map((paragraph, index) => (
 					<ToggleFeatures
 						feature="isAppRedesigned"
+						key={index}
 						on={
 							<Text
 								key={paragraph}
